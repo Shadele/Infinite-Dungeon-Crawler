61ac26acabdf4e3d0eb136e8bd8b276570cdef82
Shadele <Shadelegrier@gmail.com>
2022-03-25 13:10:58 -0400

    working on combat

--- base_game.py
+++ base_game.py
@@ -2,8 +2,8 @@
 
 from random import randint, choices #random elements
 from time import sleep #implement delays so the player isnt overwhelmed by text
-from character_generator import char_gen #character creation
-from enemy_generator import RandomEncounterMonster #monster creation
+from character_generator import PlayerCharacter, char_gen #character creation
+from enemy_generator import Enemy, RandomGeneratorMonster #monster creation
 from ending_generator import death, win #custom endings
 
 play_game = True #i want to play a game
@@ -15,3 +15,6 @@ def main():
     char_gen()
     while play_game:
         print("Welcome to the Infinite Dungeon!")
+        print(f"You are a {PlayerCharacter.race}, {PlayerCharacter.type}.")
+        print("Your goal is to find the fabulous and mysterious Amulet of Serpents!")
+        print("One of these monsters must have it...")
--- enemy_generator.py
+++ enemy_generator.py
@@ -12,7 +12,7 @@ class Enemy:
 		self.mp = mp
 		self.level = level
 
-def RandomEncounterMonster():
+def RandomGeneratorMonster():
     monster_races = ("human", "elf", "vampire", "orc", "dwarf", "lizardfolk", "ratkin", "goblin", "drow", "troll")
     monster_classes = ("fighter", "archer", "wizard", "druid", "necromancer", "assassin", "swordsman", "thief", "cultist", "nightwalker", "paladin", "fallen paladin")
     aggression = ("friendly", "neutral", "hostile")
@@ -21,3 +21,21 @@ def RandomEncounterMonster():
     aggression_level = choice(aggression)
     monster = Enemy(enemy_race, enemy_class, aggression_level, randint(80,110), randint(80, 110), range(player.level,player.level+3))
     return monster
+
+def MonsterEncounter(monster):
+    print(f"You have encountered a {monster.race}, {monster.type}.")
+    print(f"It looks to be level {monster.level}.")
+    if monster.aggression != "hostile":
+        print(f"It looks {monster.aggression}.")
+    else:
+        print("It looks hostile...")
+        while player.hp > 0:
+            print("It attacks!")
+            damage = randint(1, 5) + monster.level
+            player.hp -= damage
+            if player.hp > 0:
+                print(f"You take {damage} damage.\nYou now have {player.hp} HP remaining.")
+            else:
+                print("You die...")
+                break
+            print("Do you wish to 1. Fight?\n2. Flee?")
